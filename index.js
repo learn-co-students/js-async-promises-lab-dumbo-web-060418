2758ed2540517d3b74527646b2beca54afc92ce1
robertexe <robtabares92@gmail.com>
2018-07-19 13:43:02 -0400

    Done.

--- index.js
+++
@@ -1,7 +0,0 @@
-const questions = [
-  {questionText: "Lightning never strikes in the same place twice", answer: false},
-  {questionText: "Humans can distinguish between over one trillion different smells", answer: true},
-  {questionText: "Goldfish only have a memory of about three seconds", answer: false}
-]
-
-let question;
